581fd782c56a98bf22301461a2b08033cfe61b67
linlinlinlinlinkele <303106418@qq.com>
2025-03-27 22:37:13 +0800

    Remove old files and update structure

--- reproduction/util.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def GcsDir():
-    return os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-
-def FindModelFile(filename):
-    assert filename[:6] == "models"
-    return os.path.join(GcsDir(), filename)
